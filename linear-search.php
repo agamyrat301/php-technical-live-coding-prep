f4e3d0899c5d629d48197687fc3a065eb88fb8b8
Agamyrat <agajan301@gmail.com>
2026-05-16 03:15:28 +0300

    linear search

---
+++ linear-search.php
@@ -0,0 +1,20 @@
+<?php
+
+// implement linear search
+$array1 = [
+    'testhere',
+    'Alicia',
+    'Muhammet',
+    'Aman',
+    'ymam'
+];
+
+function linearSearch(array $arrays,string $keyword){
+    foreach ($arrays as $arr) {
+        if($arr === $keyword){
+            return $arr.PHP_EOL;
+        }
+    }
+}
+
+echo linearSearch($array1,'Aman');
